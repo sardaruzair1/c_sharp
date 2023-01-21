4b86f5b2b837b0b822e081f2bdc1fc36ed6c70b3
sardaruzair1 <sardarsafeer012012@gmail.com>
2023-01-21 21:56:07 +0500

    tutorial 01

---
+++ tut_1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello World");
+            Console.WriteLine(name);
+            Console.WriteLine("First C Sharp Program");
+            Console.WriteLine(3 + 3);
+           
+        }
+    }
+}
